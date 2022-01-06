cab16b15ee78978c9793d287312aad5775509998
tyfahpundo <tafadzwaallentyfah@gmail.com>
2022-01-06 16:38:35 +0200

    Added Feign Interface to open up endpoints from different service

--- src/main/java/zw/co/afrosoft/productservice/restclients/CouponClient.java
+++ src/main/java/zw/co/afrosoft/productservice/restclients/CouponClient.java
@@ -1,8 +1,12 @@
 package zw.co.afrosoft.productservice.restclients;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import zw.co.afrosoft.productservice.domain.Coupon;
 
 @FeignClient("COUPON-SERVICE")
 public interface CouponClient {
-
+    @GetMapping("/coupons/{code}")
+    Coupon getCoupon(@PathVariable String code);
 }
