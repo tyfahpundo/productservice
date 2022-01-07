e90947f5cbe7ee9ba5575baa2b19663a11f47363
tyfahpundo <tafadzwaallentyfah@gmail.com>
2022-01-07 11:24:12 +0200

    Adjusted the FeignClient to point to the Gateway

--- src/main/java/zw/co/afrosoft/productservice/restclients/CouponClient.java
+++ src/main/java/zw/co/afrosoft/productservice/restclients/CouponClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import zw.co.afrosoft.productservice.domain.Coupon;
 
-@FeignClient("COUPON-SERVICE")
+@FeignClient("GATEWAY-SERVICE")
 public interface CouponClient {
     @GetMapping("api/coupons/{code}")
     Coupon getCoupon(@PathVariable String code);
